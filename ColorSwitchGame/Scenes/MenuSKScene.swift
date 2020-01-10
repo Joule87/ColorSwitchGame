fcf7ca8a6fd0b33f42960077dbec63a2383aa67c
Julio Collado <jcollado@altimetrik.com>
2020-01-10 15:40:44 -0300

    ColorSwitchGame: Testing access tocken

---
+++ ColorSwitchGame/Scenes/MenuSKScene.swift
@@ -0,0 +1,13 @@
+//
+//  MenuSKScene.swift
+//  ColorSwitchGame
+//
+//  Created by Julio Collado on 1/10/20.
+//  Copyright © 2020 Julio Collado. All rights reserved.
+//
+
+import SpriteKit
+
+class MenuSKScene: SKScene {
+    
+}
